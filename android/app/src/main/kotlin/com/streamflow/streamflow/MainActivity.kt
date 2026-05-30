46a95f13f95d2ad720200e29ace724b9c039d71b
Sofien Chm <sofien.dk@gmail.com>
2026-05-30 17:13:32 +0200

    fix: android audio_service needs FlutterFragmentActivity + manifest service

--- android/app/src/main/kotlin/com/streamflow/streamflow/MainActivity.kt
+++ android/app/src/main/kotlin/com/streamflow/streamflow/MainActivity.kt
@@ -1,5 +1,5 @@
 package com.streamflow.streamflow
 
-import io.flutter.embedding.android.FlutterActivity
+import io.flutter.embedding.android.FlutterFragmentActivity
 
-class MainActivity : FlutterActivity()
+class MainActivity : FlutterFragmentActivity()
